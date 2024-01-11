c73b86a546da64d43e53b57a1f422a146e62d63f
Meky Febriansyah <mekyfebriansyah175@gmail.com>
2024-01-11 14:47:38 +0700

    menambahkan detail blog

--- resources/views/detailBlog.blade.php
+++ resources/views/detailBlog.blade.php
@@ -11,43 +11,60 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Jost">
     <title>Detail Blog</title>
 </head>
-<div id="detailBlog">
-    <div id="header">
-        <div id="judulheader">
-            ExploreNusantara
+<body>
+    <div id="detailBlog">
+        <div id="header">
+            <div id="judulheader">
+                ExploreNusantara
+            </div>
+            <div id="navigasiBlog">
+            <div class="berandaBlog">
+                <a href="">
+                    Beranda
+                </a>
+            </div>    
+            <div class="blogBlog">
+                <a href="">
+                    Blog
+                </a>
+            </div>
+            <div class="wisataBlog">
+                <a href="">
+                    Wisata
+                </a>
+            </div>
+            <div class="profilBlog">
+                <a href="">
+                    Profil
+                </a>
+            </div>
+            <div class="keluarBlog">
+                <a href="">
+                    Keluar
+                </a>
+            </div>
+            </div>
         </div>
-        <div id="navigasiBlog">
-        <div class="berandaBlog">
-            <a href="">
-                Beranda
-            </a>
-        </div>    
-        <div class="blogBlog">
-            <a href="">
-                Blog
-            </a>
+        <div class="kotakDatadetailBlog">
+            <div class="nilaiKotakDatadetailBlog">15</div>
+            <div class="deskripsiKotakDatadetailBlog">bulan yang</div>
+            <div class="deskripsiKotakDatadetailBlog">lalu</div>
+        <div class="kotakdetailBlog">
+            <div class="tanggaldetailBlog">26 Desember 2022</div>
+            <div class="juduldetailBlog">Keindahan Magis Tari Kecak: Suara, Gerakan, dan Kekuatan Bali yang Memukau
+                <div class="teksjuduldetailBlog">
+                    Di atas latar senja pulau Bali yang memesona, Tari Kecak menjadi bagian tak terpisahkan dari pesona budaya yang kaya. Dengan gerakan-gerakan yang mengagumkan dan harmoni suara yang kuat, pertunjukan ini memberikan pengalaman yang luar biasa bagi setiap penonton yang hadir.
+                    <br><br>
+                    Tarian ini bukan sekadar pertunjukan, tetapi juga cerita epik yang hidup dari zaman kuno, khususnya cerita Ramayana. Dengan menggunakan nyanyian dan gerakan tangan para penari, Kecak menjadi simbol kekuatan, perjuangan, dan kemenangan dari cerita klasik ini. Satu hal yang menakjubkan dari Tari Kecak adalah suara yang tercipta dari puluhan bahkan ratusan penari yang bersama-sama menghasilkan harmoni vokal yang menggetarkan hati. Tanpa menggunakan alat musik, suara yang dihasilkan menciptakan aura magis yang memenuhi ruang pertunjukan.
+                    <br><br>
+                    Kehadiran Tari Kecak adalah bentuk nyata dari semangat kebersamaan dan gotong royong masyarakat Bali. Melalui tarian ini, mereka menyatukan kekuatan dan semangat untuk melestarikan warisan budaya yang begitu berharga. Tari Kecak bukan hanya sekadar pertunjukan tari, tetapi juga sebuah perayaan dari warisan budaya Bali yang mempesona. Dengan pesona yang memikat dan pesan moral yang mendalam, Tari Kecak mempersembahkan keindahan dan kekuatan budaya Bali yang tak ternilai.
+                </div>
+            </div>
         </div>
-        <div class="wisataBlog">
-            <a href="">
-                Wisata
-            </a>
         </div>
-        <div class="profilBlog">
-            <a href="">
-                Profil
-            </a>
-        </div>
-        <div class="keluarBlog">
-            <a href="\">
-                Keluar
-            </a>
-        </div>
+        <div class="gambardetailBlog">
+            <img src="" alt="">
         </div>
     </div>
-    <div class="kotakDataWelcome">
-        <div class="nilaiKotakDataWelcome">56</div>
-        <div class="deskripsiKotakDataWelcome">Wisata Terdata</div>
-    </div>
-</div>
 </body>
 </html>
