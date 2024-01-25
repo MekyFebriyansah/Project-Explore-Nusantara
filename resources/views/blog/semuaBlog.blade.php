f602c66e87b0c66a8fd676ca40e23b0211648b94
Meky Febriansyah <mekyfebriansyah175@gmail.com>
2024-01-25 09:05:02 +0700

    menambahkan fitur pencarian

--- resources/views/blog/semuaBlog.blade.php
+++ resources/views/blog/semuaBlog.blade.php
@@ -19,6 +19,53 @@
         <div class="tekssemuaBlog">
             Dari pantai-pantai tropis yang memukau hingga pegunungan yang menakjubkan, melalui perjalanan visual dan naratif yang memukau, mengungkapkan keindahan panorama alamnya yang menakjubkan, dari pantai-pantai mempesona hingga gunung-gung megah yang memikat. Kami juga akan memperkenalkan Anda pada kekayaan budaya Indonesia, dari tradisi yang kuno hingga keunikan kuliner yang lezat.
         </div>
+        <div class="searchsemuaBlog">
+            <div class="isiPencarianKiri">
+                <div class="logoGambarPencarianKiri">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 23 23" fill="none">
+                        <path d="M8.28906 2.03326V4.78326" stroke="#3D6299" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M15.623 2.03326V4.78326" stroke="#3D6299" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M4.16406 8.53247H19.7474" stroke="#3D6299" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M20.2061 7.9916V15.7833C20.2061 18.5333 18.8311 20.3666 15.6227 20.3666H8.28939C5.08105 20.3666 3.70605 18.5333 3.70605 15.7833V7.9916C3.70605 5.2416 5.08105 3.40826 8.28939 3.40826H15.6227C18.8311 3.40826 20.2061 5.2416 20.2061 7.9916Z" stroke="#3D6299" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M11.9523 12.7583H11.9606" stroke="#3D6299" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M8.55877 12.7583H8.567" stroke="#3D6299" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M8.55877 15.5083H8.567" stroke="#3D6299" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </div>
+                <div class="tulisanTanggal">
+                    <div class="tulisanTanggalAtas">Tanggal <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M11.7488 5.42078L7.94546 9.22411C7.49629 9.67328 6.76129 9.67328 6.31212 9.22411L2.50879 5.42078" fill="white"/>
+                        <path d="M11.7488 5.42078L7.94546 9.22411C7.49629 9.67328 6.76129 9.67328 6.31212 9.22411L2.50879 5.42078" stroke="#808080" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                        </div>
+                    <div class="tulisanTanggalBawah">04 augs 2024</div>
+                </div>
+                <div class="logoGambarPencarianKiri">
+                    <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M11.1286 12.5108C12.7081 12.5108 13.9886 11.2303 13.9886 9.65077C13.9886 8.07124 12.7081 6.79077 11.1286 6.79077C9.54902 6.79077 8.26855 8.07124 8.26855 9.65077C8.26855 11.2303 9.54902 12.5108 11.1286 12.5108Z" fill="white" stroke="#3D6299" stroke-width="1.5"/>
+                        <path d="M3.44714 7.98243C5.25297 0.0440996 17.0138 0.0532664 18.8105 7.9916C19.8646 12.6483 16.968 16.5899 14.4288 19.0283C12.5863 20.8066 9.6713 20.8066 7.81964 19.0283C5.28964 16.5899 2.39297 12.6391 3.44714 7.98243Z" fill="white" stroke="#3D6299" stroke-width="1.5"/>
+                        </svg>
+                        
+                </div>
+                <div class="tulisanTanggal">
+                    <div class="tulisanTanggalAtas">Lokasi <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M11.7488 5.42078L7.94546 9.22411C7.49629 9.67328 6.76129 9.67328 6.31212 9.22411L2.50879 5.42078" fill="white"/>
+                        <path d="M11.7488 5.42078L7.94546 9.22411C7.49629 9.67328 6.76129 9.67328 6.31212 9.22411L2.50879 5.42078" stroke="#808080" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                        </div>
+                    <div class="tulisanTanggalBawah">Papua, Papua Barat</div>
+                </div>
+            </div>
+            <div id="tombolPencarian">
+                <button type="submit">
+                <svg xmlns="http://www.w3.org/2000/svg" width="49" height="49" viewBox="0 0 49 49" fill="none">
+                    <circle cx="24.2148" cy="24.2" r="24" fill="#3D6299"/>
+                    <path d="M11.7148 21.95C6.06484 21.95 1.46484 17.35 1.46484 11.7C1.46484 6.04995 6.06484 1.44995 11.7148 1.44995C17.3648 1.44995 21.9648 6.04995 21.9648 11.7C21.9648 17.35 17.3648 21.95 11.7148 21.95ZM11.7148 2.94995C6.88484 2.94995 2.96484 6.87995 2.96484 11.7C2.96484 16.52 6.88484 20.45 11.7148 20.45C16.5448 20.45 20.4648 16.52 20.4648 11.7C20.4648 6.87995 16.5448 2.94995 11.7148 2.94995Z" fill="white"/>
+                    <path d="M22.2153 22.95C22.0253 22.95 21.8353 22.88 21.6853 22.73L19.6853 20.73C19.3953 20.44 19.3953 19.96 19.6853 19.67C19.9753 19.38 20.4553 19.38 20.7453 19.67L22.7453 21.67C23.0353 21.96 23.0353 22.44 22.7453 22.73C22.5953 22.88 22.4053 22.95 22.2153 22.95Z" fill="white"/>
+                </svg>
+                </button>
+            </div>
+        </div>
         <div id="rumahKonten">
             @foreach($blogs as $blog)
                 <div class="kotakKonten">
